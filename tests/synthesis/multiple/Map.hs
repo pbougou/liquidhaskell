c61ba32d7994927ae2c472b45e466d853fe18a2c
Panagiotis Bougoulias <panagiotis.bougoulias@imdea.org>
2020-11-27 21:07:05 +0200

    Turn into polymorphic.

--- tests/synthesis/multiple/Map.hs
+++ tests/synthesis/multiple/Map.hs
@@ -4,11 +4,11 @@ module Map where
 
 import Language.Haskell.Liquid.Synthesize.Error
 
-{-@ myMap :: (Int -> Bool) -> xs:[Int] -> {v:[Bool] | len v == len xs} @-}
-myMap :: (Int -> Bool) -> [Int] -> [Bool]
+{-@ myMap :: (a -> b) -> xs:[a] -> {v:[b] | len v == len xs} @-}
+myMap :: (a -> b) -> [a] -> [b]
 myMap f xs0 = 
     case xs0 of 
-        [] -> _empty
+        [] -> []
         (x:xs) -> _other
 -- map f [] = []
 -- map f (x:xs) = f x : map f xs
